6f280fd8c698698eeb9ca0db114ed0ccaf98ed50
Thiago <thiagoifnmgsi2015@gmail.com>
2024-11-08 18:05:19 -0300

    Cria pasta images e atualiza README.md.

--- src/Esteganografia.py
+++ src/Esteganografia.py
@@ -1,10 +1,10 @@
 from stegano import lsb
-
+"""Fazendo um teste com a lib stegano"""
 # secret = lsb.hide("./meteor_challenge_01.png", "Teste ThiagoLuz")
 # secret.save("./meteor_challenge_01_th.png")
 
-# message = lsb.reveal("./meteor_challenge_01_th.png")
+# message = lsb.reveal("./src/imagens/meteor_challenge_01_th.png")
 # print(message)
-
+"""Pelo jeito a frase oculta não foi adicionada com a lib stegano. :/ """
 message = lsb.reveal("./meteor_challenge.png")
 print(message)
